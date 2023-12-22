a26330ed9e095cadcd75104b9c662d3fe3754b7c
MinightDev <onlyayoub565@gmail.com>
2023-12-22 11:35:56 -0800

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install puppet-lint using Puppet
-
-package { 'puppet-lint':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
