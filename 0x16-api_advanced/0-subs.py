9fde8a8059cda725b8c1ec4c1909c33f424ada9a
Ayoub <onlyayoub565@gmail.com>
2024-02-13 06:34:24 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,17 @@
 """Module for task 0"""
 
 
+import requests
+
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,2 +1,1 @@
 0x16. API advanced
-
