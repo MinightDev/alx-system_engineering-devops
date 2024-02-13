5a9a7daeadba59890273643335fa4105d8892229
Ayoub <onlyayoub565@gmail.com>
2024-02-13 06:52:27 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -21,7 +21,6 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code >= 300:
         return 0
 
